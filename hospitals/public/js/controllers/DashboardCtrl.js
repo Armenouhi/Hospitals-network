377eb0cef66caeaca29e049a33fd0b178f0e55ce
unknown <armenuhitoroyan1993@gmail.com>
2018-12-12 02:06:12 +0400

    Centre's rating

--- hospitals/public/js/controllers/DashboardCtrl.js
+++ hospitals/public/js/controllers/DashboardCtrl.js
@@ -1,33 +1,52 @@
-app.controller('DashboardController', function ($scope,$http) {
+app.controller('DashboardController', function ($scope,$http,$rootScope) {
 
-var chart = new CanvasJS.Chart("chartContainer", {
-    theme: "light2", // "light1", "light2", "dark1", "dark2"
-    exportEnabled: true,
-    animationEnabled: true,
-    title: {
-        text: "Desktop Browser Market Share in 2016"
-    },
-    data: [{
-        type: "pie",
-        startAngle: 25,
-        toolTipContent: "<b>{label}</b>: {y}%",
-        showInLegend: "true",
-        legendText: "{label}",
-        indexLabelFontSize: 16,
-        indexLabel: "{label} - {y}%",
-        dataPoints: [
-            { y: 51.08, label: "Chrome" },
-            { y: 27.34, label: "Internet Explorer" },
-            { y: 10.62, label: "Firefox" },
-            { y: 5.02, label: "Microsoft Edge" },
-            { y: 4.07, label: "Safari" },
-            { y: 1.22, label: "Opera" },
-            { y: 0.44, label: "Others" }
-        ]
-    }]
-});
-chart.render();
 
+    $http.get("/find_centers")
+        .then(function(response) {
+            $scope.centersData = response.data;
+            console.log($scope.centersData);
+
+
+            for (var i  in  $scope.centersData) {
+
+                var info = $scope.centersData[i].centreName;
+                console.log(info);
+
+                var chart = new CanvasJS.Chart("chartContainer", {
+                    theme: "dark1", // "light1", "light2", "dark1", "dark2"
+                    exportEnabled: true,
+                    animationEnabled: true,
+                    title: {
+                        text: "Desktop Browser Market Share in 2016"
+                    },
+                    data: [{
+                        type: "pie",
+                        startAngle: 25,
+                        toolTipContent: "<b style='color: red'>{label}</b>: {y}%",
+                        showInLegend: "true",
+                        legendText: "{label}",
+                        indexLabelFontSize: 16,
+                        indexLabel: "{label} - {y}%",
+                        dataPoints: [
+                            { y: 51.08, label: "Chrome" },
+                            { y: 27.34, label: "Internet Explorer" },
+                            { y: 10.62, label: "Firefox" },
+                            { y: 5.02, label: "Microsoft Edge" },
+                            { y: 4.07, label: "Safari" },
+                            { y: 1.22, label: "Opera" },
+                            { y: 0.44, label: "Others" }
+                               // { y: 5.08, label: info}
+                        ]
+                    }]
+                });
+
+                chart.render();
+            }
+
+        })
+        .catch(function(response) {
+           console.error('Gists error', response.status);
+        })
 
 
 
